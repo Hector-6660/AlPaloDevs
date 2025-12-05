3c29b5ebab95f8c363f50edcfaf14ee6e586ad73
Hector-6660 <1910132@alu.murciaeduca.es>
2025-12-05 14:31:06 +0100

    Cambios en cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5174', 'http://127.0.0.1:5174', 'https://alpalodevs.net'],
+    'allowed_origins' => [
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
+        'https://hector-6660.github.io/',
+        'https://alpalodevs.net',
+        'htttps://www.alpalodevs.net'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
