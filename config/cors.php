3776ee42864b74a3d440043c171217d499b683b1
Hector-6660 <1910132@alu.murciaeduca.es>
2025-12-05 14:35:37 +0100

    Cambios en cors (corregir sintaxis)

--- config/cors.php
+++ config/cors.php
@@ -6,9 +6,9 @@ return [
     'allowed_origins' => [
         'http://localhost:5174',
         'http://127.0.0.1:5174',
-        'https://hector-6660.github.io/',
+        'https://hector-6660.github.io',
         'https://alpalodevs.net',
-        'htttps://www.alpalodevs.net'
+        'https://www.alpalodevs.net'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
